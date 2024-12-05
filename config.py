740759c1a95573d08da0b70c72f9601f7699359e
trungnb160186 <trungnbbkno1@gmail.com>
2024-12-05 15:54:33 +0700

    update some code

--- config.py
+++ config.py
@@ -4,16 +4,16 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:
     DEBUG = True
-    POSTGRES_URL="udacitytrungnb12project3.postgres.database.azure.com" 
-    POSTGRES_USER="trungnb12" 
-    POSTGRES_PW="@Vu25062019r"   
+    POSTGRES_URL="techconfdb.postgres.database.azure.com" 
+    POSTGRES_USER="phuhb1project3" 
+    POSTGRES_PW="@Vu25062019"   
     POSTGRES_DB="techconfdb"   
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://udacitytrungnb12project3.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ZM4SzkhAaRkhQOJS5MlYdUzXM59BzG2oQ+ASbIQgJYw=' #TODO: Update value
-    SERVICE_BUS_QUEUE_NAME ='queue-project3'
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://phuhb1project3.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=fYH4RP/nG8vQjBvD5WvUX3cfwBU4MBFNI+ASbMRTo9Y=' #TODO: Update value
+    SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
     SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
 
